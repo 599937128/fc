e7cdd2f20362aaac2d650eadf96982cfd1dc45b0
lvshao <599937128@163.com>
2020-06-08 22:40:47 +0800

    代码优化

--- src/main/java/com/lv/web/kaptcha/KaptchaController.java
+++ src/main/java/com/lv/web/kaptcha/KaptchaController.java
@@ -8,7 +8,6 @@ package com.lv.web.kaptcha;
 
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
